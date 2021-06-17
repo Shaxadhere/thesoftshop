907cd94b5e5fab91dae9d2aef4332a8c8e453df8
Shaxadhere <shaxad.here@gmail.com>
2021-06-17 19:33:13 +0500

    assets

---
+++ assets/vendor/phprapid/func/charts.php
@@ -0,0 +1,72 @@
+<?php
+
+// * PHP Rapid
+// * https://github.com/Shaxadhere/phprapid
+// *
+// * Tested on PHP 7.4
+// *
+// * Copyright Shehzad Ahmed 
+// * https://shaxad.netlify.app
+// * https://github.com/Shaxadhere
+
+// * Released under the GNU GENERAL PUBLIC LICENSE
+// * 
+// *
+// * Date: 2020-08-23
+
+/**
+ * generates chart in png or gif file
+ * @param Integer  $height  expects chart height
+ * @param Integer  $width  expects chart width
+ * @param Array   $data  expects chart data in array
+ * @param Array   $labels  expects chart labels in array
+ * @param Array   $colors  expects chart colors in array
+ * @param Bool   $animate  expects chart animation in boolean
+ * 
+ * @return String chart image url
+ */ 
+function generateChart($height, $width, array $data, array $labels, array $colors, bool $animate){
+
+    //breaking data array//
+    $chartData = "";
+    $c = 0;
+    foreach ($data as $value) {
+        $chartData .= $value;
+        $c++;
+        if($c < count($data)){
+            $chartData .= ",";
+        }
+    }
+
+    //breaking labels array//
+    $chartLabels = "";
+    $c1 = 0;
+    foreach ($labels as $value1) {
+        $chartLabels .= $value1;
+        $c1++;
+        if($c1 < count($labels)){
+            $chartLabels .= "|";
+        }
+    }
+    
+    //breaking colors array//
+    $chartColors = "";
+    $c2 = 0;
+    foreach ($colors as $value2) {
+        $chartColors .= "ps0-".$c2.",lg,45,$value2,0.2,$value2,1";
+        $c2++;
+        if($c2 < count($colors)){
+            $chartColors .= "|";
+        }
+    }
+
+    //setting animation//
+    $chartAnimation = "";
+    if($animate){
+        $chartAnimation .= "chan&";
+    }
+    
+    $chartUrl = "https://image-charts.com/chart?chs=".$width."x".$height."&chd=t:".$chartData."&cht=p3&chl=".$chartLabels."&".$chartAnimation."chf=$chartColors";
+
+    return $chartUrl;
+}
---
+++ assets/vendor/phprapid/func/plays.php
@@ -0,0 +1,69 @@
+<?php
+
+// * PHP Rapid
+// * https://github.com/Shaxadhere/phprapid
+// *
+// * Tested on PHP 7.4
+// *
+// * Copyright Shehzad Ahmed 
+// * https://shaxad.netlify.app
+// * https://github.com/Shaxadhere
+
+// * Released under the GNU GENERAL PUBLIC LICENSE
+// * 
+// *
+// * Date: 2020-08-23
+
+/**
+ * adds header with dynamic title/
+ *
+ * @param String   $pageName  expects page name
+ * @param String   $headerPath  header.php path
+ * 
+ */ 
+function getHeader(string $pageName, string $headerPath)
+{
+    ob_start(); 
+    include($headerPath);
+    //include("header.php");
+    $buffer=ob_get_contents();
+    ob_end_clean();
+    $title = $pageName;
+    $buffer = preg_replace('/(<title>)(.*?)(<\/title>)/i', '$1' . $title . ' - Quiz Web$3', $buffer);
+
+    echo $buffer;
+}
+
+/**
+ * adds footer/
+ *
+ * @param String   $footerPath  expects footer.php path
+ * 
+ */ 
+function getFooter(string $footerPath){
+    include($footerPath);
+}
+
+/**
+ * redirects page with javascript
+ *
+ * @param String   $url  expects url
+ * 
+ */ 
+function redirectWindow(string $url)
+{
+    echo "<script>window.location.href='$url';</script>";
+}
+
+/**
+ * shows alert with javascript
+ *
+ * @param String   $msg  expects message
+ * 
+ */ 
+function showAlert(string $msg)
+{
+    echo "<script>alert('$msg');</script>";
+}
+
+?>
