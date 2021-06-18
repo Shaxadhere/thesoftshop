db22ae53dedf407600449676eddab8bfb46305c9
Shehzad Ahmed <shaxad.here@gmail.com>
2021-06-18 12:30:02 +0500

    banners

--- components/category-banner.php
+++ components/category-banner.php
@@ -5,10 +5,10 @@
             <div class="cat_grid_item cat_space_item cat_grid_item_1 col-md-6 col-12">
                 <div class="cat_grid_item__content position-relative oh">
                     <a href="shop-left-sidebar.html" class="db cat_grid_item__link">
-                        <div class="cat_grid_item__overlay item__position nt_bg_lz lazyload top" data-bgset="<?= getHTMLRoot() ?>/assets/images/home-01/bn-01.jpg"></div>
+                        <div class="cat_grid_item__overlay item__position nt_bg_lz lazyload top" data-bgset="<?= getHTMLRoot() ?>/uploads/banners/scrunchie2.png"></div>
                     </a>
                     <div class="cat_grid_item__wrapper pe_none">
-                        <div class="cat_grid_item__title">Women</div>
+                        <div class="cat_grid_item__title">Accessories</div>
                     </div>
                 </div>
             </div>
@@ -19,10 +19,10 @@
                     <div class="cat_grid_item cat_space_item cat_grid_item_2 col-12">
                         <div class="cat_grid_item__content position-relative oh">
                             <a href="shop-left-sidebar.html" class="db cat_grid_item__link">
-                                <div class="cat_grid_item__overlay item__position nt_bg_lz lazyload center" data-bgset="<?= getHTMLRoot() ?>/assets/images/home-01/bn-02.jpg"></div>
+                                <div class="cat_grid_item__overlay item__position nt_bg_lz lazyload center" data-bgset="<?= getHTMLRoot() ?>/uploads/banners/stationary1.jfif"></div>
                             </a>
                             <div class="cat_grid_item__wrapper pe_none">
-                                <div class="cat_grid_item__title">Accessories</div>
+                                <div class="cat_grid_item__title">Stationary</div>
                             </div>
                         </div>
                     </div>
@@ -30,10 +30,10 @@
                     <div class="cat_grid_item cat_space_item cat_grid_item_3 col-12">
                         <div class="cat_grid_item__content position-relative oh">
                             <a href="shop-left-sidebar.html" class="db cat_grid_item__link">
-                                <div class="cat_grid_item__overlay item__position nt_bg_lz lazyload center" data-bgset="<?= getHTMLRoot() ?>/assets/images/home-01/bn-03.jpg"></div>
+                                <div class="cat_grid_item__overlay item__position nt_bg_lz lazyload center" data-bgset="<?= getHTMLRoot() ?>/uploads/banners/stickers3.jfif"></div>
                             </a>
                             <div class="cat_grid_item__wrapper pe_none">
-                                <div class="cat_grid_item__title">Footwear</div>
+                                <div class="cat_grid_item__title">Stickers</div>
                             </div>
                         </div>
                     </div>
@@ -43,10 +43,10 @@
             <div class="cat_grid_item cat_space_item cat_grid_item_4  col-md-3 col-6">
                 <div class="cat_grid_item__content position-relative oh">
                     <a href="shop-left-sidebar.html" class="db cat_grid_item__link">
-                        <div class="cat_grid_item__overlay item__position nt_bg_lz lazyload center" data-bgset="<?= getHTMLRoot() ?>/assets/images/home-01/bn-04.jpg"></div>
+                        <div class="cat_grid_item__overlay item__position nt_bg_lz lazyload center" data-bgset="<?= getHTMLRoot() ?>/uploads/banners/jewellery2.jfif"></div>
                     </a>
                     <div class="cat_grid_item__wrapper pe_none">
-                        <div class="cat_grid_item__title">Watches</div>
+                        <div class="cat_grid_item__title">Jewellery</div>
                     </div>
                 </div>
             </div>
