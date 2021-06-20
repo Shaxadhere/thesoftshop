4bbf928887e7ede9f922bdf4c9a13fdc209a19aa
Shaxadhere <shaxad.here@gmail.com>
2021-06-20 20:45:14 +0500

    inventory

--- control-panel/inventory.php
+++ control-panel/inventory.php
@@ -7,7 +7,7 @@ getHeader("Inventory", "includes/header.php");
         <div>
             <nav aria-label="breadcrumb">
                 <ol class="breadcrumb breadcrumb-style1 mg-b-10">
-                    <li class="breadcrumb-item"><a href="<?= getHTMLRoot()."/dashboard" ?>">Dashboard</a></li>
+                    <li class="breadcrumb-item"><a href="<?= getHTMLRoot() . "/dashboard" ?>">Dashboard</a></li>
                     <li class="breadcrumb-item active" aria-current="page">Inventory</li>
                 </ol>
             </nav>
@@ -15,34 +15,66 @@ getHeader("Inventory", "includes/header.php");
     </div>
     <div class="container-fluid">
         <h2>Inventory</h2>
-        <div data-label="Inventory" class="main-table">
-            <table id="main-table" class="table">
+        <div data-label="Inventory" class="normal-table">
+            <table id="normal-table" class="table">
                 <thead>
                     <tr>
-                        <th class="wd-20p">Name</th>
-                        <th class="wd-25p">Position</th>
-                        <th class="wd-20p">Office</th>
-                        <th class="wd-15p">Age</th>
-                        <th class="wd-20p">Salary</th>
+                        <th class="wd-5p">S.No</th>
+                        <th class="wd-25p">Product Name</th>
+                        <th class="wd-20p">Quantity</th>
+                        <th class="wd-20p">Options</th>
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td>Tiger Nixon</td>
-                        <td>System Architect</td>
-                        <td>Edinburgh</td>
-                        <td>61</td>
-                        <td>$320,800</td>
-                    </tr>
-                    <tr>
-                        <td>Garrett Winters</td>
-                        <td>Accountant</td>
-                        <td>Tokyo</td>
-                        <td>63</td>
-                        <td>$170,750</td>
-                    </tr>
+                    <?php
+                    include_once('models/inventory-model.php');
+                    $InventoryModel = new Inventory();
+                    $InventoryList = $InventoryModel->List();
+                    $SNo = 1;
+                    while ($row = mysqli_fetch_array($InventoryList)) {
+                    ?>
+                        <tr>
+                            <td><?= $SNo ?></td>
+                            <td><?= $row['ProductName'] ?></td>
+                            <td><?= $row['Quantity'] ?></td>
+                            <td>
+                                <button class="btn btn-link dropdown-toggle" type="button" id="dropleftMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                    Options
+                                </button>
+                                <div class="dropdown-menu">
+                                    <a class="dropdown-item text-primary" href="#AddQuantity" data-toggle="modal">Add Quantity</a>
+                                    <div class="wd-200 pd-15">
+                                        <p><strong>Last Updated By: </strong>Admin</p>
+                                        <p class="mb-0"><strong>Last Updated At: </strong><?= $row['CreatedAt'] ?></p>
+                                    </div>
+                                </div>
+                            </td>
+                        </tr>
+                    <?php
+                        $SNo++;
+                    }
+                    ?>
                 </tbody>
             </table>
+        </div>
+    </div>
+</div>
+<div class="modal fade" id="AddQuantity" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel4" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
+        <div class="modal-content tx-14">
+            <div class="modal-header">
+                <h6 class="modal-title" id="exampleModalLabel4">Modal Title</h6>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <p class="mg-b-0">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. </p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary tx-13" data-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary tx-13">Save changes</button>
+            </div>
         </div>
     </div>
 </div>
