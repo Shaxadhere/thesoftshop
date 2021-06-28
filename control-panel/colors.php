1081c459db514e4c40197ba29f854d2f0227c59e
Shehzad Ahmed <shaxad.here@gmail.com>
2021-06-28 15:08:19 +0500

    Update colors.php

--- control-panel/colors.php
+++ control-panel/colors.php
@@ -16,6 +16,16 @@ getHeader("Colors", "includes/header.php");
     <?php
     HTMLToast();
     ?>
+    <div id='success-alert' class='container-fluid' style="display:none">
+        <div class='alert alert-success d-flex' role='alert'>
+            <i data-feather='alert-circle' class='mg-r-10'></i> <span id="success-alert-msg"></span>
+        </div>
+    </div>
+    <div id='error-alert' class='container-fluid' style="display:none">
+        <div class='alert alert-danger d-flex' role='alert'>
+            <i data-feather='alert-triangle' class='mg-r-10'></i> <span id="error-alert-msg"></span>
+        </div>
+    </div>
     <div class="container-fluid">
         <div class="card">
             <div class="card-body">
@@ -28,39 +38,39 @@ getHeader("Colors", "includes/header.php");
                         </div>
                         <div class="form-group col-md-12">
                             <label for="CategorySlug">Color Code</label><br>
-                            <input type="hidden" name="ColorCode" id="ColorCode" value=""/>
+                            <input type="hidden" name="ColorCode" id="ColorCode" value="" />
                             <input type="text" class="form-control" id="colorpicker">
                         </div>
                     </div>
-                    <button name="SaveCategory" type="submit" class="btn btn-primary">Submit</button>
+                    <button name="CreateColor" id="CreateColor" type="button" class="btn btn-primary">Submit</button>
                 </form>
             </div>
         </div>
 
         <hr>
-        <h2>Categories</h2>
+        <h2>Colors</h2>
         <div data-label="Categories" class="normal-table">
             <table id="normal-table" class="table">
                 <thead>
                     <tr>
                         <th class="wd-5p">S.NO</th>
-                        <th class="wd-25p">Category Name</th>
-                        <th class="wd-20p">Category Slug</th>
+                        <th class="wd-25p">Color Name</th>
+                        <th class="wd-20p">Color</th>
                         <th class="wd-20p">Options</th>
                     </tr>
                 </thead>
                 <tbody>
                     <?php
-                    include_once('models/category-model.php');
-                    $CategoryModel = new Category();
-                    $CategoryList = $CategoryModel->List();
+                    include_once('models/color-model.php');
+                    $ColorModel = new Color();
+                    $ColorList = $ColorModel->List();
                     $SNo = 1;
-                    while ($row = mysqli_fetch_array($CategoryList)) {
+                    while ($row = mysqli_fetch_array($ColorList)) {
                     ?>
                         <tr>
                             <td><?= $SNo ?></td>
-                            <td><?= $row['CategoryName'] ?></td>
-                            <td><?= $row['CategorySlug'] ?></td>
+                            <td><?= $row['ColorName'] ?></td>
+                            <td><div class="square" style="border: 1px solid lightgrey;height: 30px;width: 30px;<?= $row['ColorCode'] ?>;"></div></td>
                             <td>
                                 <button class="btn btn-link dropdown-toggle" type="button" id="dropleftMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                     Options
@@ -88,9 +98,41 @@ getHeader("Colors", "includes/header.php");
 getFooter("includes/footer.php");
 ?>
 <script>
-    $(document).on('change', '#colorpicker', function(){
+    $(document).on('change', '#colorpicker', function() {
         var ColorCode = $('.sp-preview-inner').attr("style")
         $('#ColorCode').val(ColorCode)
-        console.log($('#ColorCode').val())
+    })
+    $(document).on('click', '#CreateColor', function() {
+        var colorName = $('#ColorName').val()
+        var colorCode = $('#ColorCode').val()
+        $.ajax({
+            type: "POST",
+            url: "controllers/Color",
+            data: {
+                CreateColor: true,
+                ColorName: colorName,
+                ColorCode: colorCode
+            },
+            success: function(response) {
+                if (response == true) {
+                    $('#success-alert-msg').html("Color added successfully!")
+                    $('#success-alert').show()
+                    setTimeout(function() {
+                        $('#success-alert').hide()
+                    }, 3000)
+
+                } else {
+                    var result = JSON.parse(response)
+                    $('#error-alert-msg').html(result[0])
+                    $('#error-alert').show()
+                    setTimeout(function() {
+                        $('#error-alert').hide()
+                    }, 3000)
+                }
+            },
+            error: function(error) {
+                console.log("Error in connection: " + error)
+            }
+        })
     })
 </script>
