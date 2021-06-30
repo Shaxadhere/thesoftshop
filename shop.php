286a46481f2f6b3aa70f17a9de2ce16ee9393609
Shehzad Ahmed <shaxad.here@gmail.com>
2021-07-01 01:08:37 +0500

    Update shop.php

--- shop.php
+++ shop.php
@@ -159,7 +159,7 @@ getHeader("Shop", "includes/header.php");
                         <div class="col-lg-3 col-md-3 col-6 pr_animated done mt__30 pr_grid_item product nt_pr desgin__1">
                             <div class="product-inner pr">
                                 <div class="product-image pr oh lazyload">
-                                    <a class="d-block" href="product-detail-layout-01.html">
+                                    <a class="d-block" href="<?= getHTMLRoot()?>/view-product?name=<?= $row['ProductSlug'] ?>">
                                         <div class="pr_lazy_img main-img nt_img_ratio nt_bg_lz lazyload padding-top__127_571" data-bgset="<?= getHTMLRoot() ?>/uploads/product-images/<?= $ProductImages[0] ?>"></div>
                                     </a>
                                     <div class="hover_img pa pe_none t__0 l__0 r__0 b__0 op__0">
@@ -178,7 +178,7 @@ getHeader("Shop", "includes/header.php");
                                 </div>
                                 <div class="product-info mt__15">
                                     <h3 class="product-title pr fs__14 mg__0 fwm">
-                                        <a class="cd chp" href="product-detail-layout-01.html"><?= $row['ProductName'] ?></a>
+                                        <a class="cd chp" href="<?= getHTMLRoot()?>/view-product?name=<?= $row['ProductSlug'] ?>"><?= $row['ProductName'] ?></a>
                                     </h3>
                                     <span class="price dib mb__5">Rs. <?= $row['Price'] ?></span>
                                 </div>
