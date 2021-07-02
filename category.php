e673751aa500a0f10bb0292fb86a30378f7fe5ba
Shehzad Ahmed <shaxad.here@gmail.com>
2021-07-02 19:17:29 +0500

    category and new arrivals pagination

--- category.php
+++ category.php
@@ -22,87 +22,187 @@ getHeader($Category['CategoryName'], "includes/header.php");
 <div class="nt_section type_featured_collection tp_se_cdt">
     <div class="kalles-otp-01__feature container">
 
-        <div class="products nt_products_holder row fl_center row_pr_1 cdt_des_5 round_cd_true nt_cover ratio_nt position_8 space_30">
+        <!--products list-->
+        <div class="on_list_view_false products nt_products_holder row fl_center row_pr_1 cdt_des_1 round_cd_false nt_cover ratio_nt position_8 space_30 nt_default">
             <?php
-            include_once('web-config.php');
             include_once('models/product-model.php');
             $ProductModel = new Product();
-            $CategoryProducts = $ProductModel->ListByCategoryName($Category['CategoryName']);
-            while ($row = mysqli_fetch_array($CategoryProducts)) {
-                $Categories = json_decode($row['Categories']);
+
+            $CurrentPage = 1;
+            if (isset($_REQUEST['page'])) {
+                $CurrentPage = $_REQUEST['page'];
+            }
+            $ProductIndex = (intval($CurrentPage) * 4) - 4;
+            if ($CurrentPage == 1) {
+                $ProductIndex = 0;
+            }
+
+            $Products = $ProductModel->List($ProductIndex, 4, "", $_REQUEST['name'], "new-to-old");
+            while ($row = mysqli_fetch_array($Products)) {
                 $ProductImages = json_decode($row['ProductImages']);
-
-                $ProductDetails = $ProductModel->FilterWithAttributesByProductID(base64_encode($row['PK_ID']));
-                $Colors = array();
-                $ColorCodes = array();
-                $Sizes = array();
-
-                while ($Deatil = mysqli_fetch_array($ProductDetails)) {
-                    array_push($Colors, $Deatil['ColorName']);
-                    array_push($ColorCodes, $Deatil['ColorCode']);
-                    array_push($Sizes, $Deatil['SizeValue']);
-                }
             ?>
-
                 <div class="col-lg-3 col-md-3 col-6 pr_animated done mt__30 pr_grid_item product nt_pr desgin__1">
                     <div class="product-inner pr">
-                        <div class="product-image position-relative oh lazyload">
-
+                        <div class="product-image pr oh lazyload">
                             <a class="d-block" href="<?= getHTMLRoot() ?>/view-product?name=<?= $row['ProductSlug'] ?>">
                                 <div class="pr_lazy_img main-img nt_img_ratio nt_bg_lz lazyload padding-top__127_571" data-bgset="<?= getHTMLRoot() ?>/uploads/product-images/<?= $ProductImages[0] ?>"></div>
                             </a>
                             <div class="hover_img pa pe_none t__0 l__0 r__0 b__0 op__0">
-                                <div class="pr_lazy_img back-img pa nt_bg_lz lazyload padding-top__127_571" data-bgset="<?= getHTMLRoot() ?>/uploads/product-images/<?= (isset($ProductImages[1])) ? $ProductImages[1] : $ProductImages[0] ?>"></div>
+                                <div class="pr_lazy_img back-img pa nt_bg_lz lazyload padding-top__127_571" data-bgset="<?= getHTMLRoot() ?>/uploads/product-images/<?= isset($ProductImages[1]) ? $ProductImages[1] : $ProductImages[0] ?>"></div>
                             </div>
                             <div class="nt_add_w ts__03 pa ">
-                                <a href="#" class="wishlistadd cb chp ttip_nt tooltip_right">
-                                    <span class="tt_txt">Add to Wishlist</span>
-                                    <i class="facl facl-heart-o"></i>
-                                </a>
+                                <a href="#" class="wishlistadd cb chp ttip_nt tooltip_right"><span class="tt_txt">Add to Wishlist</span><i class="facl facl-heart-o"></i></a>
                             </div>
-                            <div class="hover_button op__0 tc pa flex column ts__03 checklol">
-                                <a class="pr nt_add_qv js_add_qv cd br__40 pl__25 pr__25 bgw tc dib ttip_nt tooltip_top_left" href="#">
-                                    <span class="tt_txt">Quick view</span>
-                                    <i data-id="<?= base64_encode($row['PK_ID']) ?>" class="iccl iccl-eye quick-view-product"></i>
-                                    <span>Quick view</span>
-                                </a>
-                                <a href="#" class="pr pr_atc cd br__40 bgw tc dib js__qs cb chp ttip_nt tooltip_top_left" class="quick-shop-product">
-                                    <span class="tt_txt">Quick Shop</span>
-                                    <i class="iccl iccl-cart"></i>
-                                    <span>Quick Shop</span>
-                                </a>
+                            <div class="hover_button op__0 tc pa flex column ts__03">
+                                <a data-id="<?= base64_encode($row['PK_ID']) ?>" class="pr nt_add_qv js_add_qv cd br__40 pl__25 pr__25 bgw tc dib ttip_nt tooltip_top_left quick-view-product" href="#"><span class="tt_txt">Quick view</span><i class="iccl iccl-eye"></i><span>Quick view</span></a>
+                                <a href="#" class="pr pr_atc cd br__40 bgw tc dib js__qs cb chp ttip_nt tooltip_top_left"><span class="tt_txt">Quick Shop</span><i class="iccl iccl-cart"></i><span>Quick Shop</span></a>
                             </div>
                             <div class="product-attr pa ts__03 cw op__0 tc">
                                 <p class="truncate mg__0 w__100">S, M, L</p>
                             </div>
                         </div>
                         <div class="product-info mt__15">
-                            <h3 class="product-title position-relative fs__14 mg__0 fwm">
+                            <h3 class="product-title pr fs__14 mg__0 fwm">
                                 <a class="cd chp" href="<?= getHTMLRoot() ?>/view-product?name=<?= $row['ProductSlug'] ?>"><?= $row['ProductName'] ?></a>
                             </h3>
                             <span class="price dib mb__5">Rs. <?= $row['Price'] ?></span>
-                            <div class="swatch__list_js swatch__list lh__1 nt_swatches_on_grid">
-                                <?php
-                                for ($i = 0; $i < count($Colors); $i++) {
-                                    echo "<span ";
-                                    echo "class='lazyload nt_swatch_on_bg swatch__list--item position-relative ttip_nt tooltip_top_right'>";
-                                    echo "<span class='tt_txt'>" . $Colors[$i] . "</span>";
-                                    echo "<span class='swatch__value' style='" . $ColorCodes[$i] . "'></span></span>";
-                                }
-                                ?>
-                            </div>
                         </div>
                     </div>
                 </div>
-
             <?php
             }
             ?>
         </div>
+        <!--end products list-->
 
+        <!--navigation-->
         <div class="products-footer tc mt__40">
-            <a class="se_cat_lm pr nt_cat_lm button button_default br_rd_true btn_icon_false" href="#">Load More</a>
+            <nav class="nt-pagination w__100 tc paginate_ajax">
+                <ul class="pagination-page page-numbers">
+                    <li>
+                        <?php
+                        $CurrentPage = 1;
+                        if (isset($_REQUEST['page'])) {
+                            $CurrentPage = $_REQUEST['page'];
+                        }
+
+                        //Pagination values
+                        $Products = $ProductModel->List(0, 9999999, "", $_REQUEST['name'], "new-to-old");
+                        $NumberOfProducts = mysqli_num_rows($Products);
+                        $PageNumbers = (intval($NumberOfProducts) / 4) + 1;
+
+                        //Previous Button URL
+                        if (isset($_REQUEST['page'])) {
+                            $page = $_REQUEST['page'];
+                            $URL = $_SERVER['QUERY_STRING'];
+                            $PrevURL = str_replace("page=$page", "page=" . intval($page) - 1, $URL);
+                        } else {
+                            $URL = $_SERVER['QUERY_STRING'];
+                            if (empty($URL)) {
+                                $PrevURL = "page=2";
+                            } else {
+                                $PrevURL = $URL . "&page=2";
+                            }
+                        }
+
+                        if ($CurrentPage == 1) {
+                            echo "<li>";
+                            echo "<span class='prev page-numbers' style='color:grey'>Prev</span>";
+                            echo "</li>";
+                        } else {
+                            echo "<li>";
+                            echo "<a class='prev page-numbers' href='" . getHTMLRoot() . "/category?$PrevURL'>Prev</a>";
+                            echo "</li>";
+                        }
+
+                        //Pagination
+                        if (isset($_REQUEST['page'])) {
+                            $page = $_REQUEST['page'];
+                            $URL = $_SERVER['QUERY_STRING'];
+                            $NewUrl = str_replace("&page=$page", "&page=" . intval($page) + 1, $URL);
+                            if (empty($URL)) {
+                                echo "<script>alert('asdsa')</script>";
+                                $NewUrl = str_replace("page=$page", "page=" . intval($page) + 1, $URL);
+                            }
+                        } else {
+                            $page = 1;
+                            $URL = $_SERVER['QUERY_STRING'];
+                            if (empty($URL)) {
+                                $NewUrl = $URL . "page=2";
+                            } else {
+                                $NewUrl = $URL . "&page=2";
+                            }
+                        }
+
+                        for ($i = 1; $i < $PageNumbers; $i++) {
+
+                            if (isset($_REQUEST['page'])) {
+                                $NewUrl = str_replace("page=$page", "page=" . $i, $URL);
+                            } else {
+                                if (empty($URL)) {
+                                    $NewUrl = $URL . "page=" . $i;
+                                } else {
+                                    $NewUrl = $URL . "&page=" . $i;
+                                }
+                            }
+
+                            if (isset($_REQUEST['page'])) {
+                                if ($_REQUEST['page'] == $i) {
+                                    echo "<li><a href='" . getHTMLRoot() . "/category?$NewUrl' class='page-numbers current'>$i</a></li>";
+                                } else {
+                                    echo "<li><a href='" . getHTMLRoot() . "/category?$NewUrl' class='page-numbers'>$i</a></li>";
+                                }
+                            } else {
+                                if ($i == "1") {
+                                    echo "<li><a href='" . getHTMLRoot() . "/category?$NewUrl' class='page-numbers current'>$i</a></li>";
+                                } else {
+                                    echo "<li><a href='" . getHTMLRoot() . "/category?$NewUrl' class='page-numbers'>$i</a></li>";
+                                }
+                            }
+                        }
+                        $NextURL = getHTMLRoot() . "/category?" . $URL;
+                        if (isset($_REQUEST['page'])) {
+                            if ($_REQUEST['page'] != $NumberOfProducts) {
+                                $PageNumber = intval($_REQUEST['page']) + 1;
+                                $NextURL .= "page=" . $PageNumber;
+                            }
+                        } else {
+                            if (1 != $NumberOfProducts) {
+                                $PageNumber = 2;
+                                $NextURL .= "page=" . $PageNumber;
+                            }
+                        }
+
+                        //Next Button URL
+                        if (isset($_REQUEST['page'])) {
+                            $page = $_REQUEST['page'];
+                            $URL = $_SERVER['QUERY_STRING'];
+                            $NextURL = str_replace("page=$page", "page=" . intval($page) + 1, $URL);
+                        } else {
+                            $URL = $_SERVER['QUERY_STRING'];
+                            if (empty($URL)) {
+                                $NextURL = "page=2";
+                            } else {
+                                $NextURL = $URL . "&page=2";
+                            }
+                        }
+
+
+
+                        if ($CurrentPage == intval($PageNumbers)) {
+                            echo "<li>";
+                            echo "<span class='next page-numbers' style='color:grey'>Next</span>";
+                            echo "</li>";
+                        } else {
+                            echo "<li>";
+                            echo "<a class='next page-numbers' href='" . getHTMLRoot() . "/category?$NextURL'>Next</a>";
+                            echo "</li>";
+                        }
+                        ?>
+                </ul>
+            </nav>
         </div>
+        <!--end navigation-->
     </div>
 </div>
 <!-- end featured collection -->
