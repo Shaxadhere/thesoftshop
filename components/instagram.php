fb36673e56adfbe4ce6eebe9be4777705507e934
Shehzad Ahmed <shaxad.here@gmail.com>
2021-07-02 20:09:01 +0500

    instagram

--- components/instagram.php
+++ components/instagram.php
@@ -3,7 +3,7 @@
     <div class="nt_full">
         <div class="wrap_title mb__20 des_title_2">
             <h3 class="section-title tc pr flex fl_center al_center fs__24 title_2">
-                <span class="mr__10 ml__10">@ FOLLOW US ON INSTAGRAM</span>
+                <span class="mr__10 ml__10">@TheSoftShop.PK ON INSTAGRAM</span>
             </h3>
             <span class="dn tt_divider"><span></span>
                 <i class="dn clprfalse title_2 la-gem"></i>
@@ -13,70 +13,43 @@
         </div>
         <div class="ins_shop_wrap row ins_rounded_0 ins_spaces_0 nt_cover ratio1_1 position_8 equal_nt js_carousel nt_slider prev_next_0 btn_owl_1 dot_owl_1 dot_color_1 btn_vi_1" data-flickity='{"imagesLoaded": 0,"adaptiveHeight": 1, "contain": 1, "groupCells": "100%", "dragThreshold" : 5, "cellAlign": "left","wrapAround": false,"prevNextButtons": true,"percentPosition": 1,"pageDots": false, "autoPlay" : 0, "pauseAutoPlayOnHover" : true, "rightToLeft": false }'>
             <div class="pin__wr_js col_ins col-lg-2 col-md-3 col-6 pr_animated done item pr oh">
-                <a href="#" class="js_add_qv hotspot_ins pa op__0 dark spin-position__01">
-                    <span>1</span>
-                </a>
                 <div class="wrap_ins_img db pr oh">
-                    <div class="lazyload nt_bg_lz pr_lazy_img" data-bgset="<?= getHTMLRoot() ?>/assets/images/instagram/ins1_14.jpg"></div>
+                    <div class="lazyload nt_bg_lz pr_lazy_img" data-bgset="<?= getHTMLRoot() ?>/uploads/instagram-pictures/1.jpg"></div>
                 </div>
             </div>
             <div class="pin__wr_js col_ins col-lg-2 col-md-3 col-6 pr_animated done item pr oh">
-                <a href="#" class="js_add_qv hotspot_ins pa op__0 dark spin-position__02">
-                    <span>1</span>
-                </a>
                 <div class="wrap_ins_img db pr oh">
-                    <div class="lazyload nt_bg_lz pr_lazy_img" data-bgset="<?= getHTMLRoot() ?>/assets/images/instagram/ins1_8.jpg"></div>
+                    <div class="lazyload nt_bg_lz pr_lazy_img" data-bgset="<?= getHTMLRoot() ?>/uploads/instagram-pictures/2.jpg"></div>
                 </div>
             </div>
             <div class="pin__wr_js col_ins col-lg-2 col-md-3 col-6 pr_animated done item pr oh">
-                <a href="#" class="js_add_qv hotspot_ins pa op__0 dark spin-position__03">
-                    <span>1</span>
-                </a>
-                <a href="#" class="js_add_qv hotspot_ins pa op__0 dark spin-position__04">
-                    <span>2</span>
-                </a>
                 <div class="wrap_ins_img db pr oh">
-                    <div class="lazyload nt_bg_lz pr_lazy_img" data-bgset="<?= getHTMLRoot() ?>/assets/images/instagram/ins1_1.jpg"></div>
+                    <div class="lazyload nt_bg_lz pr_lazy_img" data-bgset="<?= getHTMLRoot() ?>/uploads/instagram-pictures/6.png"></div>
                 </div>
             </div>
             <div class="pin__wr_js col_ins col-lg-2 col-md-3 col-6 pr_animated done item pr oh">
-                <a href="#" class="js_add_qv hotspot_ins pa op__0 dark spin-position__05">
-                    <span>1</span>
-                </a>
                 <div class="wrap_ins_img db pr oh">
-                    <div class="lazyload nt_bg_lz pr_lazy_img" data-bgset="<?= getHTMLRoot() ?>/assets/images/instagram/ins1_6.jpg"></div>
+                    <div class="lazyload nt_bg_lz pr_lazy_img" data-bgset="<?= getHTMLRoot() ?>/uploads/instagram-pictures/3.jpg"></div>
                 </div>
             </div>
             <div class="pin__wr_js col_ins col-lg-2 col-md-3 col-6 pr_animated done item pr oh">
-                <a href="#" class="js_add_qv hotspot_ins pa op__0 dark spin-position__06">
-                    <span>1</span>
-                </a>
                 <div class="wrap_ins_img db pr oh">
-                    <div class="lazyload nt_bg_lz pr_lazy_img" data-bgset="<?= getHTMLRoot() ?>/assets/images/instagram/ins1_5.jpg"></div>
+                    <div class="lazyload nt_bg_lz pr_lazy_img" data-bgset="<?= getHTMLRoot() ?>/uploads/instagram-pictures/4.jpg"></div>
                 </div>
             </div>
             <div class="pin__wr_js col_ins col-lg-2 col-md-3 col-6 pr_animated done item pr oh">
-                <a href="#" class="js_add_qv hotspot_ins pa op__0 dark spin-position__07">
-                    <span>1</span>
-                </a>
                 <div class="wrap_ins_img db pr oh">
-                    <div class="lazyload nt_bg_lz pr_lazy_img" data-bgset="<?= getHTMLRoot() ?>/assets/images/instagram/ins1_4.jpg"></div>
+                    <div class="lazyload nt_bg_lz pr_lazy_img" data-bgset="<?= getHTMLRoot() ?>/uploads/instagram-pictures/5.jpg"></div>
                 </div>
             </div>
             <div class="pin__wr_js col_ins col-lg-2 col-md-3 col-6 pr_animated done item pr oh">
-                <a href="#" class="js_add_qv hotspot_ins pa op__0 dark spin-position__08">
-                    <span>1</span>
-                </a>
                 <div class="wrap_ins_img db pr oh">
-                    <div class="lazyload nt_bg_lz pr_lazy_img" data-bgset="<?= getHTMLRoot() ?>/assets/images/instagram/ins1_2.jpg"></div>
+                    <div class="lazyload nt_bg_lz pr_lazy_img" data-bgset="<?= getHTMLRoot() ?>/uploads/instagram-pictures/7.png"></div>
                 </div>
             </div>
             <div class="pin__wr_js col_ins col-lg-2 col-md-3 col-6 pr_animated done item pr oh">
-                <a href="#" class="js_add_qv hotspot_ins pa op__0 dark spin-position__09">
-                    <span>1</span>
-                </a>
                 <div class="wrap_ins_img db pr oh">
-                    <div class="lazyload nt_bg_lz pr_lazy_img" data-bgset="<?= getHTMLRoot() ?>/assets/images/instagram/ins1_13.jpg"></div>
+                    <div class="lazyload nt_bg_lz pr_lazy_img" data-bgset="<?= getHTMLRoot() ?>/uploads/instagram-pictures/8.jpg"></div>
                 </div>
             </div>
         </div>
