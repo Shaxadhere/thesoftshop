ba03ed7e13d1eebcb6a4aa5409c8029c78cfbe79
Shehzad Ahmed <shaxad.here@gmail.com>
2021-07-06 16:50:44 +0500

    Update product.php

--- controllers/product.php
+++ controllers/product.php
@@ -4,17 +4,17 @@ include_once('../models/product-model.php');
 
 $ProductModel = new Product();
 
-if(isset($_POST['ViewProduct'])){
+if (isset($_POST['ViewProduct'])) {
     $errors = array();
-    if(empty($_POST['ProductID'])){
+    if (empty($_POST['ProductID'])) {
         array_push($errors, "502 - Bad Request Error");
     }
-    if($errors == null){
+    if ($errors == null) {
         $Product = $ProductModel->FilterWithAttributesByProductID(
             $_POST['ProductID']
         );
         $ProductDetailsArray = array();
-        while ($row = mysqli_fetch_array($Product)){
+        while ($row = mysqli_fetch_array($Product)) {
             array_push($ProductDetailsArray, $row);
         }
         $result = array(
@@ -22,8 +22,7 @@ if(isset($_POST['ViewProduct'])){
             "productDetails" => $ProductDetailsArray
         );
         echo json_encode($result);
-    }
-    else{
+    } else {
         $result = array(
             "success" => false,
             "error" => $errors[0]
@@ -32,52 +31,58 @@ if(isset($_POST['ViewProduct'])){
     }
 }
 
-if(isset($_POST['AddToCart'])){
+if (isset($_POST['AddToCart'])) {
     $errors = array();
+    session_start();
+    if (!isset($_SESSION['CART']) || $_SESSION['CART'] == "") {
+        $Cart = array();
+        $_SESSION['CART'] = json_encode($Cart);
+    }
 
-    if(empty($_POST['ProductID'])){
+    if (empty($_POST['ProductID'])) {
         array_push($errors, "502 - Bad request error");
     }
 
-    if(empty($_POST['Color'])){
+    if (empty($_POST['Color'])) {
         array_push($errors, "Please choose a color");
     }
 
-    if(empty($_POST['Size'])){
+    if (empty($_POST['Size'])) {
         array_push($errors, "Please choose a size");
     }
 
-    if(empty($_POST['Quantity']) || $_POST['Quantity'] == "0" || $_POST['Quantity'] == 0){
+    if (empty($_POST['Quantity']) || $_POST['Quantity'] == "0" || $_POST['Quantity'] == 0) {
         array_push($errors, "Invalid quantity");
     }
-    
-    if($errors == null){
-        $ProductID = base64_decode($_POST['ProductID']);
-        $ProductID = mysqli_real_escape_string(connect(), $ProductID);
-        $Color = mysqli_real_escape_string(connect(), $_POST['Color']);
-        $Size = mysqli_real_escape_string(connect(), $_POST['Size']);
-        $Quantity = mysqli_real_escape_string(connect(), $_POST['Quantity']);
-    
-        $CartItem = array(
-            "productId" => $ProductID,
-            "productqty" => $Quantity,
-            "productColor" => $Color,
-            "productSize" => $Size
-        );
-    
-        session_start();
-        if(!isset($_SESSION['CART']) || $_SESSION['CART'] == ""){
-            $Cart = array();
-            $_SESSION['CART'] = json_encode($Cart);
-        }
-    
-        $Cart = json_decode($_SESSION['CART']);
-        array_push($Cart, $CartItem);
-        $_SESSION['CART'] = json_encode($Cart);
-        echo true;
+
+    $ProductID = base64_decode($_POST['ProductID']);
+    $ProductID = mysqli_real_escape_string(connect(), $ProductID);
+    $Color = mysqli_real_escape_string(connect(), $_POST['Color']);
+    $Size = mysqli_real_escape_string(connect(), $_POST['Size']);
+    $Quantity = mysqli_real_escape_string(connect(), $_POST['Quantity']);
+
+    $Cart = json_decode($_SESSION['CART']);
+
+    foreach($Cart as $item){
+        $a = json_encode($item);
+        echo $a;
+        $b = json_decode($a);
+        echo $a['productId'];
+        // if($item['productId'] == $Product){
+        //     array_push($errors, "same product");
+        // }
     }
-    else{
-        echo json_encode($errors);
-    }
-    
+    // if ($errors == null) {
+    //     $CartItem = array(
+    //         "productId" => $ProductID,
+    //         "productqty" => $Quantity,
+    //         "productColor" => $Color,
+    //         "productSize" => $Size
+    //     );
+    //     array_push($Cart, $CartItem);
+    //     $_SESSION['CART'] = json_encode($Cart);
+    //     echo true;
+    // } else {
+    //     echo json_encode($errors);
+    // }
 }
