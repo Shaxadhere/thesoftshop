1c58cbb2236ebbb07aa8adbfc8483a783386d3c3
Shehzad Ahmed <shaxad.here@gmail.com>
2021-07-07 19:57:31 +0500

    mini cart box

--- cart.php
+++ cart.php
@@ -6,124 +6,123 @@ $ProductModel = new Product();
 $Cart = $_SESSION['CART'];
 ?>
 <div id="cart-root">
-<?php
-if(count($Cart) == 0){
-?>
-<div class="kalles-section page_section_heading">
-    <div class="page-head tc pr oh cat_bg_img page_head_">
-        <div class="parallax-inner nt_parallax_false lazyload nt_bg_lz pa t__0 l__0 r__0 b__0" data-bgset="assets/images/slide/banner21.jpg"></div>
-        <div class="container pr z_100">
-            <h1 class="mb__5 cw">Cart</h1>
-            <p class="mg__0"></p>
-        </div>
-    </div>
-</div>
-<div class="empty tc mt__60 mb__60"><i class="las la-shopping-bag pr mb__10"></i>
-    <p>Your cart is empty.</p>
-    <p class="return-to-shop mb__15">
-        <a class="button button_primary tu js_add_ld" href="<?= getHTMLRoot() ?>/shop">Return To Shop</a>
-    </p>
-</div>
-<?php
-}
-else{
-?>
-<!--cart section-->
-<div class="kalles-section cart_page_section container mt__60 cart-items-container">
-    <form action="<?= getHTMLRoot() ?>/checkout" method="post" class="frm_cart_ajax_true frm_cart_page nt_js_cart pr oh ">
-        <div class="cart_header">
-            <div class="row al_center">
-                <div class="col-5">Product</div>
-                <div class="col-3 tc">Price</div>
-                <div class="col-2 tc">Quantity</div>
-                <div class="col-2 tc tr_md">Total</div>
+    <?php
+    if (count($Cart) == 0) {
+    ?>
+        <div class="kalles-section page_section_heading">
+            <div class="page-head tc pr oh cat_bg_img page_head_">
+                <div class="parallax-inner nt_parallax_false lazyload nt_bg_lz pa t__0 l__0 r__0 b__0" data-bgset="assets/images/slide/banner21.jpg"></div>
+                <div class="container pr z_100">
+                    <h1 class="mb__5 cw">Cart</h1>
+                    <p class="mg__0"></p>
+                </div>
             </div>
         </div>
-        <div class="cart_items js_cat_items">
-            <?php
-            $Subtotal = 0;
-            foreach ($Cart as $cartItem) {
-            $Product = $ProductModel->FilterByProductID(base64_encode($cartItem['productId']));
-            $Product = mysqli_fetch_array($Product);
-            $ProductImages = json_decode($Product['ProductImages']);
-            ?>
-                <div class="cart_item js_cart_item">
-                    <div class="ld_cart_bar"></div>
+        <div class="empty tc mt__60 mb__60"><i class="las la-shopping-bag pr mb__10"></i>
+            <p>Your cart is empty.</p>
+            <p class="return-to-shop mb__15">
+                <a class="button button_primary tu js_add_ld" href="<?= getHTMLRoot() ?>/shop">Return To Shop</a>
+            </p>
+        </div>
+    <?php
+    } else {
+    ?>
+        <!--cart section-->
+        <div class="kalles-section cart_page_section container mt__60 cart-items-container">
+            <form action="<?= getHTMLRoot() ?>/checkout" method="post" class="frm_cart_ajax_true frm_cart_page nt_js_cart pr oh ">
+                <div class="cart_header">
                     <div class="row al_center">
-                        <div class="col-12 col-md-12 col-lg-5">
-                            <div class="page_cart_info flex al_center">
-                                <a href="product-detail-layout-01.html">
-                                    <img style="width: 140px;height: 140px;object-fit: cover;padding:5px" class="lazyload w__100 lz_op_ef" src="data:image/svg+xml,%3Csvg%20viewBox%3D%220%200%201128%201439%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%3E%3C%2Fsvg%3E" data-src="<?= getHTMLRoot() ?>/uploads/product-images/<?= $ProductImages[0] ?>" alt="">
-                                </a>
-                                <div class="mini_cart_body ml__15">
-                                    <h5 class="mini_cart_title mg__0 mb__5"><a href="product-detail-layout-01.html"><?= $Product['ProductName'] ?></a></h5>
-                                    <div class="mini_cart_meta">
-                                        <p class="cart_selling_plan">
-                                            <span>Size: <?= $cartItem['productSize'] ?></span><br>
-                                            <span>Color: <?= $cartItem['productColor'] ?></span>
-                                        </p>
-                                    </div>
-                                    <div class="mini_cart_tool mt__10">
-                                        <a data-cartItemId="<?= $cartItem['CartItemId'] ?>" href="#" class="remove-cart-item"><span class="tt_txt">Remove this item</span>
-                                        <i style="font-size: 20px;" class="pegk pe-7s-trash"></i>
+                        <div class="col-5">Product</div>
+                        <div class="col-3 tc">Price</div>
+                        <div class="col-2 tc">Quantity</div>
+                        <div class="col-2 tc tr_md">Total</div>
+                    </div>
+                </div>
+                <div class="cart_items js_cat_items">
+                    <?php
+                    $Subtotal = 0;
+                    foreach ($Cart as $cartItem) {
+                        $Product = $ProductModel->FilterByProductID(base64_encode($cartItem['productId']));
+                        $Product = mysqli_fetch_array($Product);
+                        $ProductImages = json_decode($Product['ProductImages']);
+                    ?>
+                        <div class="cart_item js_cart_item">
+                            <div class="ld_cart_bar"></div>
+                            <div class="row al_center">
+                                <div class="col-12 col-md-12 col-lg-5">
+                                    <div class="page_cart_info flex al_center">
+                                        <a href="product-detail-layout-01.html">
+                                            <img style="width: 140px;height: 140px;object-fit: cover;padding:5px" class="lazyload w__100 lz_op_ef" src="data:image/svg+xml,%3Csvg%20viewBox%3D%220%200%201128%201439%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%3E%3C%2Fsvg%3E" data-src="<?= getHTMLRoot() ?>/uploads/product-images/<?= $ProductImages[0] ?>" alt="">
                                         </a>
+                                        <div class="mini_cart_body ml__15">
+                                            <h5 class="mini_cart_title mg__0 mb__5"><a href="product-detail-layout-01.html"><?= $Product['ProductName'] ?></a></h5>
+                                            <div class="mini_cart_meta">
+                                                <p class="cart_selling_plan">
+                                                    <span>Size: <?= $cartItem['productSize'] ?></span><br>
+                                                    <span>Color: <?= $cartItem['productColor'] ?></span>
+                                                </p>
+                                            </div>
+                                            <div class="mini_cart_tool mt__10">
+                                                <a data-cartItemId="<?= $cartItem['CartItemId'] ?>" href="#" data-location="cart" class="remove-cart-item"><span class="tt_txt">Remove this item</span>
+                                                    <i style="font-size: 20px;" class="pegk pe-7s-trash"></i>
+                                                </a>
+                                            </div>
+                                        </div>
                                     </div>
                                 </div>
-                            </div>
-                        </div>
-                        <div class="col-12 col-md-4 col-lg-3 tc__ tc_lg">
-                            <div class="cart_meta_prices price">
-                                <div class="cart_price">Rs. <?= $Product['Price'] ?></div>
-                            </div>
-                        </div>
-                        <div class="col-12 col-md-4 col-lg-2 tc mini_cart_actions">
-                            <div class="quantity pr mr__10 qty__true">
-                                <input type="number" class="input-text qty text tc qty_cart_js" name="updates[]" value="<?= $cartItem['productqty'] ?>">
-                                <div class="qty tc fs__14">
-                                    <button type="button" class="plus db cb pa pd__0 pr__15 tr r__0">
-                                        <i class="facl facl-plus"></i></button>
-                                    <button type="button" class="minus db cb pa pd__0 pl__15 tl l__0 qty_1">
-                                        <i class="facl facl-minus"></i></button>
+                                <div class="col-12 col-md-4 col-lg-3 tc__ tc_lg">
+                                    <div class="cart_meta_prices price">
+                                        <div class="cart_price">Rs. <?= $Product['Price'] ?></div>
+                                    </div>
+                                </div>
+                                <div class="col-12 col-md-4 col-lg-2 tc mini_cart_actions">
+                                    <div class="quantity pr mr__10 qty__true">
+                                        <input type="number" class="input-text qty text tc qty_cart_js" name="updates[]" value="<?= $cartItem['productqty'] ?>">
+                                        <div class="qty tc fs__14">
+                                            <button type="button" class="plus db cb pa pd__0 pr__15 tr r__0">
+                                                <i class="facl facl-plus"></i></button>
+                                            <button type="button" class="minus db cb pa pd__0 pl__15 tl l__0 qty_1">
+                                                <i class="facl facl-minus"></i></button>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-12 col-md-4 col-lg-2 tc__ tr_lg">
+                                    <span class="cart-item-price fwm cd js_tt_price_it">Rs. <?= intval($Product['Price']) * intval($cartItem['productqty']) ?></span>
                                 </div>
                             </div>
                         </div>
-                        <div class="col-12 col-md-4 col-lg-2 tc__ tr_lg">
-                            <span class="cart-item-price fwm cd js_tt_price_it">Rs. <?= intval($Product['Price']) * intval($cartItem['productqty']) ?></span>
+                    <?php
+                        $Subtotal = $Subtotal + intval($Product['Price']) * intval($cartItem['productqty']);
+                    }
+                    ?>
+                </div>
+                <div class="cart__footer mt__60">
+                    <div class="row">
+                        <div class="col-12 tr_md tc order-md-12 order-12 col-md-12">
+                            <div class="total row in_flex fl_between al_center cd fs__18 tu">
+                                <div class="col-auto"><strong>Subtotal:</strong></div>
+                                <div class="col-auto tr js_cat_ttprice fs__20 fwm">
+                                    <div class="cart_tot_price">Rs. <?= $Subtotal ?></div>
+                                </div>
+                            </div>
+                            <div class="clearfix"></div>
+                            <p class="db txt_tax_ship mb__5">Taxes, shipping and discounts codes calculated at checkout</p>
+                            <div class="clearfix"></div>
+                            <button type="submit" name="update" class="button btn_update mt__10 mb__10 js_add_ld w__100">Update Cart</button>
+                            <button type="submit" onclick="location.href='<?= getHTMLRoot() ?>/checkout'" data-confirm="ck_lumise" name="checkout" class="btn_checkout button button_primary tu mt__10 mb__10 js_add_ld w__100">Check Out</button>
+                            <div class="clearfix"></div>
+                            <div class="cat_img_trust mt__10">
+                                <img class="lz_op_ef lazyload w-50" src="data:image/svg+xml,%3Csvg%20viewBox%3D%220%200%20476%2052%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%3E%3C%2Fsvg%3E" data-src="<?= getHTMLRoot() ?>/uploads/product-images/cart_image.png" alt="">
+                            </div>
                         </div>
                     </div>
                 </div>
-            <?php
-            $Subtotal = $Subtotal + intval($Product['Price']) * intval($cartItem['productqty']);
-            }
-            ?>
+            </form>
         </div>
-        <div class="cart__footer mt__60">
-            <div class="row">
-                <div class="col-12 tr_md tc order-md-12 order-12 col-md-12">
-                    <div class="total row in_flex fl_between al_center cd fs__18 tu">
-                        <div class="col-auto"><strong>Subtotal:</strong></div>
-                        <div class="col-auto tr js_cat_ttprice fs__20 fwm">
-                            <div class="cart_tot_price">Rs. <?= $Subtotal ?></div>
-                        </div>
-                    </div>
-                    <div class="clearfix"></div>
-                    <p class="db txt_tax_ship mb__5">Taxes, shipping and discounts codes calculated at checkout</p>
-                    <div class="clearfix"></div>
-                    <button type="submit" name="update" class="button btn_update mt__10 mb__10 js_add_ld w__100">Update Cart</button>
-                    <button type="submit" onclick="location.href='<?= getHTMLRoot() ?>/checkout'" data-confirm="ck_lumise" name="checkout" class="btn_checkout button button_primary tu mt__10 mb__10 js_add_ld w__100">Check Out</button>
-                    <div class="clearfix"></div>
-                    <div class="cat_img_trust mt__10">
-                        <img class="lz_op_ef lazyload w-50" src="data:image/svg+xml,%3Csvg%20viewBox%3D%220%200%20476%2052%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%3E%3C%2Fsvg%3E" data-src="<?= getHTMLRoot() ?>/uploads/product-images/cart_image.png" alt="">
-                    </div>
-                </div>
-            </div>
-        </div>
-    </form>
-</div>
-<!--end cart section-->
-<?php
-}
-?>
+        <!--end cart section-->
+    <?php
+    }
+    ?>
 </div>
 <?php
 getFooter("includes/footer.php");
--- components/mini-cart-box.php
+++ components/mini-cart-box.php
@@ -12,8 +12,9 @@ $Cart = $_SESSION['CART'];
         </div>
         <div class="mini_cart_wrap">
             <div class="mini_cart_content fixcl-scroll">
-                <div class="fixcl-scroll-content">
+                <div class="fixcl-scroll-content" id="cart-root">
                     <?php
+                    $Subtotal = 0;
                     if (count($Cart) == 0) {
                     ?>
                         <div class="empty tc mt__40"><i class="las la-shopping-bag pr mb__10"></i>
@@ -27,13 +28,12 @@ $Cart = $_SESSION['CART'];
                     ?>
                         <div class="mini_cart_items js_cat_items lazyload">
                             <?php
-                            $Subtotal = 0;
                             foreach ($Cart as $cartItem) {
                                 $Product = $ProductModel->FilterByProductID(base64_encode($cartItem['productId']));
                                 $Product = mysqli_fetch_array($Product);
                                 $ProductImages = json_decode($Product['ProductImages']);
                             ?>
-                                <div class="mini_cart_item js_cart_item flex al_center pr oh">
+                                <div class="mini_cart_item js_cart_item flex al_center pr oh cart_item">
                                     <div class="ld_cart_bar"></div>
                                     <a href="<?= getHTMLRoot() ?>/view-product?name=<?= $Product['ProductSlug'] ?>" class="mini_cart_img">
                                         <img class="w__100 lazyload" data-src="<?= getHTMLRoot() ?>/uploads/product-images/<?= $ProductImages[0] ?>" width="120" height="153" alt="" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIxMjAiIGhlaWdodD0iMTUzIiB2aWV3Qm94PSIwIDAgMTIwIDE1MyI+PC9zdmc+">
@@ -44,41 +44,24 @@ $Cart = $_SESSION['CART'];
                                             <p class="cart_meta_variant"><?= $cartItem['productSize'] ?> | <?= $cartItem['productColor'] ?></p>
                                             <div class="cart_meta_price price">
                                                 <div class="cart_price">
-                                                    <p class="price_range" id="price_ppr">Rs. <?= intval($Product['Price']) * intval($cartItem['productqty']) ?></p>
+                                                    <p class="price_range" id="price_ppr">Quantity: <strong><?= $cartItem['productqty'] ?></strong></p>
+                                                    <p class="price_range" id="price_ppr">Price: Rs. <?= $Product['Price'] ?></p>
+                                                    <p class="price_range" id="price_ppr"><strong>Total: Rs. <?= intval($Product['Price']) * intval($cartItem['productqty']) ?></strong></p>
                                                 </div>
                                             </div>
                                         </div>
                                         <div class="mini_cart_actions">
-                                            <div class="quantity pr mr__10 qty__true">
-                                                <input type="number" class="input-text qty text tc qty_cart_js" step="1" min="0" max="9999" value="<?= $cartItem['productqty'] ?>">
-                                                <div class="qty tc fs__14">
-                                                    <button type="button" class="plus db cb pa pd__0 pr__15 tr r__0">
-                                                        <i class="facl facl-plus"></i>
-                                                    </button>
-                                                    <button type="button" class="minus db cb pa pd__0 pl__15 tl l__0 qty_1">
-                                                        <i class="facl facl-minus"></i>
-                                                    </button>
-                                                </div>
-                                            </div>
-                                            <a href="#" class="cart_ac_edit js__qs ttip_nt tooltip_top_right"><span class="tt_txt">Edit this item</span>
-                                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                                                    <path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"></path>
-                                                    <path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"></path>
-                                                </svg>
+                                            <a data-cartItemId="<?= $cartItem['CartItemId'] ?>" href="<?= getHTMLRoot() ?>/cart" class="remove-cart-item"><span class="tt_txt">Edit this item</span>
+                                                <i style="font-size: 20px;" class="pegk pe-7s-note"></i>
                                             </a>
-                                            <a href="#" class="cart_ac_remove js_cart_rem ttip_nt tooltip_top_right"><span class="tt_txt">Remove this item</span>
-                                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                                                    <polyline points="3 6 5 6 21 6"></polyline>
-                                                    <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path>
-                                                    <line x1="10" y1="11" x2="10" y2="17"></line>
-                                                    <line x1="14" y1="11" x2="14" y2="17"></line>
-                                                </svg>
+                                            <a data-cartItemId="<?= $cartItem['CartItemId'] ?>" href="#" data-location="mini-cart" class="remove-cart-item"><span class="tt_txt">Remove this item</span>
+                                                <i style="font-size: 20px;" class="pegk pe-7s-trash"></i>
                                             </a>
                                         </div>
                                     </div>
                                 </div>
                             <?php
-                            $Subtotal = $Subtotal + intval($Product['Price']) * intval($cartItem['productqty']);
+                                $Subtotal = $Subtotal + intval($Product['Price']) * intval($cartItem['productqty']);
                             }
                             ?>
                         </div>
