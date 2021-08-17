4d94054dca0750b016e6c992e6bb95621d014abd
Shehzad Ahmed <shaxad.here@gmail.com>
2021-08-17 16:19:20 +0500

    Update summarized-counts.php

--- control-panel/components/summarized-counts.php
+++ control-panel/components/summarized-counts.php
@@ -3,6 +3,7 @@ include_once('web-config.php');
 include_once('models/order-model.php');
 $OrderModel = new Order();
 $Orders = $OrderModel->List();
+
 $OrdersCount = mysqli_num_rows($Orders);
 $LastWeekOrders = $OrderModel->ListFromDateToDate(date('Y-m-d', strtotime('-7 days')), date('Y-m-d'));
 $LastWeekOrdersCount = mysqli_num_rows($LastWeekOrders);
@@ -12,6 +13,28 @@ while($row = mysqli_fetch_array($FrequentCities)){
     array_push($FrequentCity, $row['CustomerCity']);
 }
 
+$SalesAmount = 0;
+foreach ($Orders as $row) {
+    $TotalProductsWithQuantity = json_decode($row['ProductsWithQuantity'], true);
+    $Bill = 0;
+    foreach ($TotalProductsWithQuantity as $item) {
+        $Bill = $Bill + (intval($item['ProductQuantity']) * intval($item['PricePerUnit']));
+    }
+    $SalesAmount = $SalesAmount + $Bill;
+}
+
+include_once('models/investments-model.php');
+
+$InvestmentModel = new Investment();
+$fetched = $InvestmentModel->List();
+
+$MaryamInv = $InvestmentModel->InvCount(2);
+$MaryamInv = mysqli_fetch_array($MaryamInv);
+
+$ShehzadInv = $InvestmentModel->InvCount(1);
+$ShehzadInv = mysqli_fetch_array($ShehzadInv);
+
+$TotalInvestments = intval($MaryamInv[0]) + intval($ShehzadInv[0]);
 ?>
 <div class="col-sm-6 col-lg-3 mg-t-10 mg-lg-t-0">
     <div class="card card-body">
@@ -42,29 +65,17 @@ while($row = mysqli_fetch_array($FrequentCities)){
 </div>
 <div class="col-sm-6 col-lg-3">
     <div class="card card-body">
-        <h6 class="tx-uppercase tx-11 tx-spacing-1 tx-color-02 tx-semibold mg-b-8">Conversion Rate</h6>
+        <h6 class="tx-uppercase tx-11 tx-spacing-1 tx-color-02 tx-semibold mg-b-8">Sales Amount</h6>
         <div class="d-flex d-lg-block d-xl-flex align-items-end">
-            <h3 class="tx-normal tx-rubik mg-b-0 mg-r-5 lh-1">0.81%</h3>
-            <p class="tx-11 tx-color-03 mg-b-0">
-                <span class="tx-medium tx-success">1.2%
-                    <i class="icon ion-md-arrow-up"></i>
-                </span>
-                than last week
-            </p>
+            <h3 class="tx-primary tx-rubik mg-b-0 mg-r-5 lh-1">PKR <?= $SalesAmount ?></h3>
         </div>
     </div>
 </div>
 <div class="col-sm-6 col-lg-3 mg-t-10 mg-sm-t-0">
     <div class="card card-body">
-        <h6 class="tx-uppercase tx-11 tx-spacing-1 tx-color-02 tx-semibold mg-b-8">Unique Purchases</h6>
+        <h6 class="tx-uppercase tx-11 tx-spacing-1 tx-color-02 tx-semibold mg-b-8">Total Investments</h6>
         <div class="d-flex d-lg-block d-xl-flex align-items-end">
-            <h3 class="tx-normal tx-rubik mg-b-0 mg-r-5 lh-1">3,137</h3>
-            <p class="tx-11 tx-color-03 mg-b-0">
-                <span class="tx-medium tx-danger">0.7%
-                    <i class="icon ion-md-arrow-down"></i>
-                </span>
-                than last week
-            </p>
+            <h3 class="tx-normal tx-rubik mg-b-0 mg-r-5 lh-1">PKR <?= $TotalInvestments ?></h3>
         </div>
     </div>
 </div>
