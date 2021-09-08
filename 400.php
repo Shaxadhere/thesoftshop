1ee0109a56c149452ed73a3ec069aeec3bee3270
Shehzad Ahmed <shaxad.here@gmail.com>
2021-09-08 12:19:38 +0500

    error pages

--- 400.php
+++ 400.php
@@ -1,14 +1,14 @@
 <?php
-include_once('../web-config.php');
+include_once('web-config.php');
 getHeader(
-    "400 - Bad Request Error",//page title
-    "../includes/header.php",//header path
+    "Bad Request Error",//page title
+    "includes/header.php",//header path
     "Error Page",//pagetype
     "buy scrunchies in pakistan, buy vintage potraits in pakistan, buy stickers in pakistan, buy nostalgic vintage accessories in pakistan",//page keywords
     "Explore your aesthetic, Buy scrunchies, stickers, nostalgic vintage accessories in pakistan",//description
-    "400 - Bad Request Error",//topic
+    "Bad Request Error",//topic
     'http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']//url
 );
-include_once('../components/mobile-menu.php');
-getFooter("../includes/footer.php");
+include_once('components/mobile-menu.php');
+getFooter("includes/footer.php");
 ?>
